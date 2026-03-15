d2839c88ecd0cad86326e0d2deb1e9a693a29a57
Ebuka24Stephen <sebuka559@gmail.com>
2026-03-15 13:07:36 +0100

    configure redis as broker

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -160,7 +160,7 @@ RESEND_API_KEY=config("RESEND_API_KEY")
 DEFAULT_FROM_EMAIL = "onboarding@resend.dev"
 
 
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 
